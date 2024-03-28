335fec278c864ccc8e637453b4ce2e7622579a00
MelvilPE <melvil.bouron@orange.fr>
2024-03-28 15:56:36 +0100

    Successfully copy content directly to table folder

--- Gen1/BackOffice/admin/GenerateCRUDS.py
+++ Gen1/BackOffice/admin/GenerateCRUDS.py
@@ -1,8 +1,12 @@
 import os
+import shutil
 import SQLTemplateReader
 
 # Path to the SQL template file used for generating CRUD
 SQL_TEMPLATE_FILENAME = "SQLTemplate.sql"
+
+TEMPLATE_FOLDER_NAME = "template"
+
 
 def GenerateCRUD(templateInfo):
     tableName = templateInfo["name"]
@@ -10,15 +14,21 @@ def GenerateCRUD(templateInfo):
 
     if os.path.isdir(tableName):
         try:
-            os.remove(tableName)
+            shutil.rmtree(tableName)
         except:
             print("[GenerateCRUDS::GenerateCRUDS] failed to remove existing folder: " + tableName)
             return
+        
+        try:
+            os.mkdir(tableName)
+        except:
+            print("[GenerateCRUDS::GenerateCRUDS] failed to re-create folder: " + tableName)
+    
     try:
-        os.mkdir(tableName)
-    except OSError:
-        print("[GenerateCRUDS::GenerateCRUDS] failed to create a new folder: " + tableName)
-
+        shutil.copytree(TEMPLATE_FOLDER_NAME, tableName)
+    except:
+        print("[GenerateCRUDS::GenerateCRUDS] failed to dupplicate to new folder: " + tableName)
+    
     return
 
 def GenerateCRUDS(templateInfos):
