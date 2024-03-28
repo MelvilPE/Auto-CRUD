05442f05407c2607c690f3f9430d16687bc899e8
MelvilPE <melvil.bouron@orange.fr>
2024-03-28 19:30:38 +0100

    Update GenerateCRUDS.py and add comments

--- Gen1/BackOffice/admin/GenerateCRUDS.py
+++ Gen1/BackOffice/admin/GenerateCRUDS.py
@@ -6,8 +6,18 @@ import CRUDFilesProcessor
 # Path to the SQL template file used for generating CRUD
 SQL_TEMPLATE_FILENAME = "SQLTemplate.sql"
 
+# Name of the folder containing the PHP template files
 TEMPLATE_FOLDER_NAME = "template"
 
+"""
+Generates CRUD for the given table template info.
+
+This function takes in a templateInfo dict containing the table name and params.
+It first checks if a folder with the table name already exists, and removes it if so. 
+
+It then copies the template folder to a new folder named after the table.
+Finally, it will process PHP files on the new folder to rewrite parts of the CRUD template.
+"""
 def GenerateCRUD(templateInfo):
     tableName = templateInfo["name"]
     tableParams = templateInfo["params"]
@@ -24,12 +34,23 @@ def GenerateCRUD(templateInfo):
     except:
         print("[GenerateCRUDS::GenerateCRUDS] failed to dupplicate to new folder: " + tableName)
 
-    CRUDFilesProcessor.ProcessFiles(tableName)
+    CRUDFilesProcessor.ProcessFiles(tableName, tableParams)
 
+"""
+Generate CRUD files for each table found in SQL template informations.
+Args:
+    templateInfos (list): The list of table template info dicts. 
+    Each dict contains the table name and column informations.
+"""
 def GenerateCRUDS(templateInfos):
     for templateIndex in range(len(templateInfos)):
         GenerateCRUD(templateInfos[templateIndex])
 
+"""
+Main entry point for the CRUD file generation. 
+Reads the SQL template, extracts the table informations
+And generates the CRUD files for each found tables in the SQL template
+"""
 def Main():
     templateInfos = SQLTemplateReader.ReadSQLTemplate(SQL_TEMPLATE_FILENAME)
     GenerateCRUDS(templateInfos)
